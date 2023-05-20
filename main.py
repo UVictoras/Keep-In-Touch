0bdbf1b5a6f287fc7f8a5f62ddb6307faa6f72d1
Victoras <vmartinan@gaming.tech>
2023-05-20 11:25:26 +0200

    timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Import librairies
 import pygame, sys
 import pygame.time
+import random as r
 
 #Init the pygame library
 pygame.init()
@@ -25,23 +26,30 @@ def test():
 def gameLoop():
     running = True
 
+    timer = r.randint(60, 300)
+    
     mainButton = Button(960, 250, BUTTON, test)
-    while running:
+    while running and timer > 0:
         SCREEN.blit(BG, (0,0))
         SCREEN.blit(mainButton.image, mainButton.rect)
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
+        clock = pygame.time.Clock()
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-                    pygame.quit()
-                    sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if mainButton.checkForInput(MENU_MOUSE_POS):
                     print("good")
 
+        timer -= 1
+
         pygame.display.update()
+        
+    pygame.quit()
+    sys.exit()
 
 gameLoop()
